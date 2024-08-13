220adb3c934ca7ac6397a71ab8b24347d3b6a368
cristhian <cristhianarayaro@gmail.com>
2024-08-13 04:04:45 -0400

    Se suben correcciones en backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 require('dotenv').config()
 
 const app = require('./src/app')
+const { database, initDatabase } = require('./src/database/dbConfig')
+
 const PORT = process.env.PORT
 
-app.listen(PORT,console.log(`Servidor corre en el PORT http://127.0.0.1:${PORT}`))
+app.listen(PORT, async () => { 
+    console.log(`Servidor corre en el PORT http://127.0.0.1:${PORT}`)
+    await database.query(initDatabase())
+    
+})
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,21 @@
+require ('dotenv').config()
 const express = require('express')
 const routes = require('./routes/route')
 const cors = require('cors')
 const morgan = require('morgan')
 const { HandleDatabaseLogs } = require ('../src/middlewares/logsMicar')
 
+
 const app = express()
+
+const {FRONT_URL} = process.env
 
 app.use(express.json())
 
 app.use(morgan('dev'))
 
  app.use(cors({
-     origin:'http://localhost:5173'
+     origin: '*'
  }))
 
  app.use('/',HandleDatabaseLogs,routes)

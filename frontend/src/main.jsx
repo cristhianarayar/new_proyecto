a21350dbba73cd93050e3ce075d5bda21cc4dc5c
cristhian <cristhianarayaro@gmail.com>
2024-08-14 01:09:22 -0400

    se sube boostrap

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import MicarProvider from './context/MicarProvider.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
